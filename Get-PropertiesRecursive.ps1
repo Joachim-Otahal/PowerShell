131fc82bed39372ae0aaac6f05d093343b4a93fc
Joachim Otahal <jou@gmx.net>
2024-05-12 13:41:03 +0200

    get all object properties listed

---
+++ Get-PropertiesRecursive.ps1
@@ -0,0 +1,47 @@
+﻿# Gets all properties recursively. Useful if you need a list of all sub-proerties.
+# V 0.1 Joachim Otahal, May 2024
+
+$Car = [PSCustomObject] @{
+    Tire          = [PSCustomObject] @{
+        Color = "Black"
+        Count = 4
+    }
+
+    SteeringWheel = [PSCustomObject]@{
+        Color   = "Blue"
+        Buttons = 15
+    }
+    'Special Decal' = [PSCustomObject]@{
+        'Decal:1' = [bool]$true
+    }
+    'Windows-Electrical'= [PSCustomObject]@{
+        Ordered_Feature = [bool]$true
+    }
+}
+
+
+function Get-PropertiesRecursive {
+    param (
+        [Parameter(ValueFromPipeline)][object]$InputObject,
+        [String]$ParentName
+    )
+    if ($ParentName) {$ParentName +="."}
+    foreach ($Property in $InputObject.psobject.Properties) {
+        # This puts special characters in '' like you need it when using
+        if ($Property.Name -like "*:*" -or $Property.Name -like "* *"  -or $Property.Name -like "*-*") {
+            $Name = "'$($Property.Name)'"
+        } else {
+            $Name = $Property.Name
+        }
+        if ($Property.TypeNameOfValue.Split(".")[-1] -ne "PSCustomObject") {
+            [pscustomobject]@{
+                TypeName = $Property.TypeNameOfValue.Split(".")[-1]
+                Property = "$ParentName$Name"
+                Value = $Property.Value
+            }
+        } else {
+            Get-PropertiesRecursive $Property.Value -ParentName "$ParentName$Name"
+        }
+    }
+}
+Get-PropertiesRecursive $Car -ParentName '$Car'
